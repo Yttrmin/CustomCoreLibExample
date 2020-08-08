303ccc319aa6226e197cacbdcc9b7cf15a7066d2
Yttrmin <Yttrmin@users.noreply.github.com>
2020-08-08 05:32:35 -0400

    Convert to .NET 5.0, try importing a regular library as well (no IDE underlines, but won't compile).

--- ConsumerTest/Class1.cs
+++ ConsumerTest/Class1.cs
@@ -8,7 +8,12 @@ namespace ConsumerTest
         {
             // We can't overload many operators for String (e.g. +), as the compiler seems to treat it
             // special (e.g. tries to expand the + operator to a Concat() call). We can add an implicit cast to int though!
-            int a = "Strings are ints." * 156;
+            
+            // We can call .NET 5.0 libraries without an underline appearing in the IDE (assuming it uses types
+            // defined in the custom core lib). But it won't actually compile because it wants a ref to System.Runtime.
+            // Calling into a .NET Standard 1.0 library will make it explode (complains/underlines about no refs to built in types).
+            // Calling into a .NET Standard 2.0-2.1 project will have a similar error to .NET 5.0 (wants ref to netstandard).
+            int a = "Strings are ints." * RegularLibrary.Class1.ReturnInt(156);
             a += 37;
             //a. // Try out Intellisense and see that you only get the methods we definied in CustomCoreLib::Object.
         }
---
+++ RegularLibrary/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RegularLibrary
+{
+    public static class Class1
+    {
+        // Call will NOT be underlined in ConsumterTest.
+        public static int ReturnInt(int value)
+        {
+            return value;
+        }
+
+        // Call WILL be underlined in ConsumerTest (CustomCoreLib has no ref to Guid).
+        public static Guid ReturnGuid()
+        {
+            return new Guid();
+        }
+    }
+}
